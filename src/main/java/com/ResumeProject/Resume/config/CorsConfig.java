ffbae08c915aa74c2eb546186478901305accefe
DESKTOP-TB3I824\admin <sainaath4763@gmail.com>
2026-02-20 12:27:45 +0530

    removed trailing slash in cors

--- src/main/java/com/ResumeProject/Resume/config/CorsConfig.java
+++ src/main/java/com/ResumeProject/Resume/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://resume-react-pi-umber.vercel.app/")
+                .allowedOrigins("https://resume-react-pi-umber.vercel.app")
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                 .allowedHeaders("*");
     }
